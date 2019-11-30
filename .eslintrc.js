0e5c6ed8519dfb6b69a57d3010bce243b33e41cd
KOREAN139 <korean139@gmail.com>
2019-11-30 13:34:08 +0900

    build(typescript): add modules for unit test
    use mocha & chai for unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'mocha',
   ],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
+    'plugin:mocha/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2018,
