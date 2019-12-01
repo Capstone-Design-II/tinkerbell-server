22bb093ba23dc5eb99dededa4349b38abad859ab
KOREAN139 <korean139@gmail.com>
2019-12-02 04:14:03 +0900

    build(mongoose): add modules for mongoose
    use mongoose (ODM for mongoDB) to interact w/ cosmos DB
    
    issue #4

--- config/config.sample.js
+++ config/config.sample.js
@@ -1,4 +1,3 @@
 module.exports.ENVS = serverless => ({
-  SLS_AWS_REGION: '<Place aws region here>',
-  SLS_AWS_CRED_PROFILE: '<Place aws credential profile here>',
+  COSMOS_DB_ENDPOINT: '<Place cosmos db endpoint here>',
 });
