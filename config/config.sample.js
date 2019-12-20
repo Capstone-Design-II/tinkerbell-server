d282faf19159b3284011524d163cce4bca7e0cd8
KOREAN139 <korean139@gmail.com>
2019-12-20 21:13:38 +0900

    feat(s3): add bucket to store customer's voice
    issue #4

--- config/config.sample.js
+++ config/config.sample.js
@@ -3,7 +3,9 @@ module.exports.ENVS = serverless => ({
   SLS_AWS_CRED_PROFILE: '<Place aws credential profile here>',
   DYNAMODB_USER_TABLE_NAME: '<Place dynamoDB table name here>',
   DYNAMODB_HISTORY_TABLE_NAME: '<Place dynamoDB table name here>',
+  S3_CUSTOMER_VOICES_BUCKET_NAME: '<Place S3 bucket name here>',
   S3_MEETING_RECORDS_BUCKET_NAME: '<Place S3 bucket name here>',
+  S3_TRANSCRIPTS_BUCKET_NAME: '<Place S3 bucket name here>',
   AZURE_COGNITIVE_SERVICE_KEY: '<Place azure cognitive service subscription key here>',
   AZURE_COGNITIVE_SERVICE_ENDPOINT: '<Place azure cognitive service endpoint here>',
 });
