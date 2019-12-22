462745ef5615c833694ac06c30bf8ab54e9a002e
KOREAN139 <korean139@gmail.com>
2019-12-22 09:07:51 +0900

    feat(enqueue): send task info to transcript queue
    send (uuid, key) pair to transcribe invoker through SQS
    
    issue #10

--- src/handler/enqueue-request.ts
+++ src/handler/enqueue-request.ts
@@ -1,4 +1,5 @@
 import { APIGatewayEvent, ProxyResult, Handler, Context } from 'aws-lambda'
+import { SQS } from 'aws-sdk'
 import { HistoryGateway } from '../entity-gateway/history-gateway'
 import { v4 as uuid } from 'uuid'
 
@@ -11,6 +12,28 @@ const putHistory: Function = async (uuid: string): Promise<void> => {
   await historyTable.putHistory(uuid)
 }
 
+const sendMessage: Function = async (
+  uuid: string,
+  key: string
+): Promise<void> => {
+  const {
+    SQS_TRANSCRIPT_QUEUE_URL
+  } = process.env
+
+  const sqs = new SQS()
+  const body = { uuid, key }
+  const sendMessageParam = {
+    QueueUrl: SQS_TRANSCRIPT_QUEUE_URL,
+    MessageBody: JSON.stringify(body),
+    MessageGroupId: uuid,
+    MessageDeduplicationId: uuid,
+  }
+
+  await sqs.sendMessage(
+    sendMessageParam as SQS.Types.SendMessageRequest
+  ).promise()
+}
+
 const generateOKResponse: Function = (uuid: string): ProxyResult => {
   const response: ProxyResult = {
     statusCode: 200,
@@ -20,14 +43,16 @@ const generateOKResponse: Function = (uuid: string): ProxyResult => {
   return response
 }
 
-const enqueue: Handler = async (_event: APIGatewayEvent, _context: Context) => {
-  // const { body } = event
-  // const requestBody = JSON.parse(body!)
+const enqueue: Handler = async (event: APIGatewayEvent, _context: Context) => {
+  const { body } = event
+  const requestBody = JSON.parse(body!)
 
   const uuid = generateUuid()
   await putHistory(uuid)
 
-  // TODO: start transcript
+  const { key } = requestBody
+  await sendMessage(uuid, key)
+
   const response = generateOKResponse(uuid)
   return response
 }
