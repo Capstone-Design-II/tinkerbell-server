c6aacd14c44e9946cde09c4831de88124ed94cc7
KOREAN139 <korean139@gmail.com>
2019-12-23 01:58:14 +0900

    feat(note): generate note with transcript result
    generate note based on transcript note - need to substitute speaker
    label into real user's name
    
    issue #17

--- src/@types/tinkerbell.d.ts
+++ src/@types/tinkerbell.d.ts
@@ -9,6 +9,52 @@ declare namespace Tinkerbell {
     bucket: string
     key: string
   }
+
+  export interface TranscriptResult {
+    jobName:   string
+    accountID: string
+    results:   Results
+    status:    string
+  }
+
+  export interface Results {
+    transcripts:    Transcript[]
+    speaker_labels: SpeakerLabels
+    items:          Item[]
+  }
+
+  export interface Item {
+    start_time?:   string
+    end_time?:     string
+    alternatives:  Alternative[]
+    type:          Type
+  }
+
+  export interface Alternative {
+    confidence: string
+    content:    string
+  }
+
+  export enum Type {
+    Pronunciation = "pronunciation",
+    Punctuation = "punctuation",
+  }
+
+  export interface SpeakerLabels {
+    speakers: number
+    segments: Segment[]
+  }
+
+  export interface Segment {
+    start_time:    string
+    speaker_label: string
+    end_time:      string
+    items?:        Segment[]
+  }
+
+  export interface Transcript {
+    transcript: string
+  }
 }
 
 declare module 'tinkerbell' {
